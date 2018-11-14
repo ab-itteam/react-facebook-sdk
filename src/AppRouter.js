77d4d2a70a7b06a864fc27719adad085acc17570
meiremans <nick@ab-it.io>
2018-11-15 00:55:17 +0200

    CNAME file

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,14 +3,11 @@ import { Route, HashRouter } from "react-router-dom";
 import App from "./App";
 import MembershipCard from "./MembershipCard";
 
-const Users = () => <h2>Users</h2>;
-
 const AppRouter = () => (
-    <HashRouter >
+    <HashRouter>
         <div>
-            <Route path="/" exact component={App} />
+            <Route path="/" component={App} />
             <Route path="/card/" component={MembershipCard} />
-            <Route path="/users/" component={Users} />
         </div>
     </HashRouter>
 );
